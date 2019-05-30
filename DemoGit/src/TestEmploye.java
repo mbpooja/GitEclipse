48329289e588693872b585e9e2efb6dab8148f9d
mbpooja <mbpooja321@gmail.com>
2019-05-30 16:53:20 +0530

    Welcome to Mphasis...

---
+++ DemoGit/src/TestEmploye.java
@@ -0,0 +1,13 @@
+
+public class TestEmploye {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employe e1=new Employe();
+		e1.setEmpid(101);
+		e1.setEmpName("pooja");
+		e1.setEmpSal(1000.0);
+
+	}
+
+}
