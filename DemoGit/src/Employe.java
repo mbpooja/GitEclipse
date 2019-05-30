63ff065b99ab4fb6628f44f9684b4df55a1aa65f
mbpooja <mbpooja321@gmail.com>
2019-05-30 16:44:07 +0530

    Employe Model

---
+++ DemoGit/src/Employe.java
@@ -0,0 +1,25 @@
+
+public class Employe {
+ private int empid;
+ private String empName;
+ private Double EmpSal;
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public Double getEmpSal() {
+	return EmpSal;
+}
+public void setEmpSal(Double empSal) {
+	EmpSal = empSal;
+}
+ 
+}
